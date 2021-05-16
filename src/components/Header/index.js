290a5d19b0e663184b96713aa98ccbb66059e75f
Aashay <[seproject94@gmail.com]>
2021-05-16 23:39:08 +0530

    custom routing

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,26 @@
 import React from 'react'
 import "./styles.scss"
 import logo from "../../assets/logo.jpg"
+import { Link } from 'react-router-dom'
 
 const Header = (props) => {
     return (
         <header className="header">
             <div className="wrap">
                 <div className="logo">
-                    <img src={logo} alt="Logo"/>
+                    <Link to="/">
+                        <img src={logo} alt="Logo"/>
+                    </Link>
                 </div>
+            </div>
+            <div className="callToActions">
+                <ul>
+                    <li>
+                        <Link to="/registration">
+                            Register
+                        </Link>
+                    </li>
+                </ul>
             </div>
         </header>
     )
